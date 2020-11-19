8dcae2ce75dd0a0280958fc94aeb3bc01ec7035b
Alexander Kropotov <alexander.kropotov@bsc.es>
2020-11-19 16:34:32 +0100

    App: Adding TLAST bit manipulation in short loopback test.

--- src/eth_test.cpp
+++ src/eth_test.cpp
@@ -139,33 +139,52 @@ int main(int argc, char *argv[])
     uint8_t const LOOPBACK_DEPTH = 52;
     printf("Transmitting data to loopback with depth %d:\n", LOOPBACK_DEPTH);
     uint32_t putData = 0xDEADBEEF;
-    uint32_t fslNrdy = 1;
-    uint32_t fslErr  = 1;
+    bool fslNrdy = true;
+    bool fslErr  = true;
 
     for (uint8_t word = 0; word < LOOPBACK_DEPTH;            word++)
     for (uint8_t chan = 0; chan < XPAR_MICROBLAZE_FSL_LINKS; chan++) {
-      // channel id goes to macro as literal 
-      if (chan==0)  putfslx(putData,  0, FSL_NONBLOCKING);
-      if (chan==1)  putfslx(putData,  1, FSL_NONBLOCKING);
-      if (chan==2)  putfslx(putData,  2, FSL_NONBLOCKING);
-      if (chan==3)  putfslx(putData,  3, FSL_NONBLOCKING);
-      if (chan==4)  putfslx(putData,  4, FSL_NONBLOCKING);
-      if (chan==5)  putfslx(putData,  5, FSL_NONBLOCKING);
-      if (chan==6)  putfslx(putData,  6, FSL_NONBLOCKING);
-      if (chan==7)  putfslx(putData,  7, FSL_NONBLOCKING);
-      if (chan==8)  putfslx(putData,  8, FSL_NONBLOCKING);
-      if (chan==9)  putfslx(putData,  9, FSL_NONBLOCKING);
-      if (chan==10) putfslx(putData, 10, FSL_NONBLOCKING);
-      if (chan==11) putfslx(putData, 11, FSL_NONBLOCKING);
-      if (chan==12) putfslx(putData, 12, FSL_NONBLOCKING);
-      if (chan==13) putfslx(putData, 13, FSL_NONBLOCKING);
-      if (chan==14) putfslx(putData, 14, FSL_NONBLOCKING);
-      if (chan==15) putfslx(putData, 15, FSL_NONBLOCKING);
+      // channel id goes to macro as literal
+      if (word%2) { // transmitting TLAST in odd words
+        if (chan==0)  putfslx(putData,  0, FSL_NONBLOCKING_CONTROL);
+        if (chan==1)  putfslx(putData,  1, FSL_NONBLOCKING_CONTROL);
+        if (chan==2)  putfslx(putData,  2, FSL_NONBLOCKING_CONTROL);
+        if (chan==3)  putfslx(putData,  3, FSL_NONBLOCKING_CONTROL);
+        if (chan==4)  putfslx(putData,  4, FSL_NONBLOCKING_CONTROL);
+        if (chan==5)  putfslx(putData,  5, FSL_NONBLOCKING_CONTROL);
+        if (chan==6)  putfslx(putData,  6, FSL_NONBLOCKING_CONTROL);
+        if (chan==7)  putfslx(putData,  7, FSL_NONBLOCKING_CONTROL);
+        if (chan==8)  putfslx(putData,  8, FSL_NONBLOCKING_CONTROL);
+        if (chan==9)  putfslx(putData,  9, FSL_NONBLOCKING_CONTROL);
+        if (chan==10) putfslx(putData, 10, FSL_NONBLOCKING_CONTROL);
+        if (chan==11) putfslx(putData, 11, FSL_NONBLOCKING_CONTROL);
+        if (chan==12) putfslx(putData, 12, FSL_NONBLOCKING_CONTROL);
+        if (chan==13) putfslx(putData, 13, FSL_NONBLOCKING_CONTROL);
+        if (chan==14) putfslx(putData, 14, FSL_NONBLOCKING_CONTROL);
+        if (chan==15) putfslx(putData, 15, FSL_NONBLOCKING_CONTROL);
+      } else {
+        if (chan==0)  putfslx(putData,  0, FSL_NONBLOCKING);
+        if (chan==1)  putfslx(putData,  1, FSL_NONBLOCKING);
+        if (chan==2)  putfslx(putData,  2, FSL_NONBLOCKING);
+        if (chan==3)  putfslx(putData,  3, FSL_NONBLOCKING);
+        if (chan==4)  putfslx(putData,  4, FSL_NONBLOCKING);
+        if (chan==5)  putfslx(putData,  5, FSL_NONBLOCKING);
+        if (chan==6)  putfslx(putData,  6, FSL_NONBLOCKING);
+        if (chan==7)  putfslx(putData,  7, FSL_NONBLOCKING);
+        if (chan==8)  putfslx(putData,  8, FSL_NONBLOCKING);
+        if (chan==9)  putfslx(putData,  9, FSL_NONBLOCKING);
+        if (chan==10) putfslx(putData, 10, FSL_NONBLOCKING);
+        if (chan==11) putfslx(putData, 11, FSL_NONBLOCKING);
+        if (chan==12) putfslx(putData, 12, FSL_NONBLOCKING);
+        if (chan==13) putfslx(putData, 13, FSL_NONBLOCKING);
+        if (chan==14) putfslx(putData, 14, FSL_NONBLOCKING);
+        if (chan==15) putfslx(putData, 15, FSL_NONBLOCKING);
+      }
       fsl_isinvalid(fslNrdy);
       fsl_iserror  (fslErr);
-      // printf("Writing word %d to FSL%d = %0lX, Full = %0lX, Err = %0lX \n", word, chan, putData, fslNrdy, fslErr);
+      // printf("Writing word %d to FSL%d = %0lX, Full = %d, Err = %d \n", word, chan, putData, fslNrdy, fslErr);
       if (fslNrdy || fslErr) {
-        printf("\nERROR: Failed write of word %d to FSL%d = %0lX, Full = %0lX, Err = %0lX \n", word, chan, putData, fslNrdy, fslErr);
+        printf("\nERROR: Failed write of word %d to FSL%d = %0lX, Full = %d, Err = %d \n", word, chan, putData, fslNrdy, fslErr);
         exit(1);
       }
       putData += chan;
@@ -184,33 +203,52 @@ int main(int argc, char *argv[])
     printf("Receiving data from loopback with depth %d:\n", LOOPBACK_DEPTH);
     uint32_t getData = 0;
     putData = 0xDEADBEEF;
-    fslNrdy = 1;
-    fslErr  = 1;
+    fslNrdy = true;
+    fslErr  = true;
 
     for (uint8_t word = 0; word < LOOPBACK_DEPTH;            word++)
     for (uint8_t chan = 0; chan < XPAR_MICROBLAZE_FSL_LINKS; chan++) {
       // channel id goes to macro as literal 
-      if (chan==0)  getfslx(getData,  0, FSL_NONBLOCKING);
-      if (chan==1)  getfslx(getData,  1, FSL_NONBLOCKING);
-      if (chan==2)  getfslx(getData,  2, FSL_NONBLOCKING);
-      if (chan==3)  getfslx(getData,  3, FSL_NONBLOCKING);
-      if (chan==4)  getfslx(getData,  4, FSL_NONBLOCKING);
-      if (chan==5)  getfslx(getData,  5, FSL_NONBLOCKING);
-      if (chan==6)  getfslx(getData,  6, FSL_NONBLOCKING);
-      if (chan==7)  getfslx(getData,  7, FSL_NONBLOCKING);
-      if (chan==8)  getfslx(getData,  8, FSL_NONBLOCKING);
-      if (chan==9)  getfslx(getData,  9, FSL_NONBLOCKING);
-      if (chan==10) getfslx(getData, 10, FSL_NONBLOCKING);
-      if (chan==11) getfslx(getData, 11, FSL_NONBLOCKING);
-      if (chan==12) getfslx(getData, 12, FSL_NONBLOCKING);
-      if (chan==13) getfslx(getData, 13, FSL_NONBLOCKING);
-      if (chan==14) getfslx(getData, 14, FSL_NONBLOCKING);
-      if (chan==15) getfslx(getData, 15, FSL_NONBLOCKING);
+      if (word%2) { // expecting TLAST in odd words
+        if (chan==0)  getfslx(getData,  0, FSL_NONBLOCKING_CONTROL);
+        if (chan==1)  getfslx(getData,  1, FSL_NONBLOCKING_CONTROL);
+        if (chan==2)  getfslx(getData,  2, FSL_NONBLOCKING_CONTROL);
+        if (chan==3)  getfslx(getData,  3, FSL_NONBLOCKING_CONTROL);
+        if (chan==4)  getfslx(getData,  4, FSL_NONBLOCKING_CONTROL);
+        if (chan==5)  getfslx(getData,  5, FSL_NONBLOCKING_CONTROL);
+        if (chan==6)  getfslx(getData,  6, FSL_NONBLOCKING_CONTROL);
+        if (chan==7)  getfslx(getData,  7, FSL_NONBLOCKING_CONTROL);
+        if (chan==8)  getfslx(getData,  8, FSL_NONBLOCKING_CONTROL);
+        if (chan==9)  getfslx(getData,  9, FSL_NONBLOCKING_CONTROL);
+        if (chan==10) getfslx(getData, 10, FSL_NONBLOCKING_CONTROL);
+        if (chan==11) getfslx(getData, 11, FSL_NONBLOCKING_CONTROL);
+        if (chan==12) getfslx(getData, 12, FSL_NONBLOCKING_CONTROL);
+        if (chan==13) getfslx(getData, 13, FSL_NONBLOCKING_CONTROL);
+        if (chan==14) getfslx(getData, 14, FSL_NONBLOCKING_CONTROL);
+        if (chan==15) getfslx(getData, 15, FSL_NONBLOCKING_CONTROL);
+      } else {
+        if (chan==0)  getfslx(getData,  0, FSL_NONBLOCKING);
+        if (chan==1)  getfslx(getData,  1, FSL_NONBLOCKING);
+        if (chan==2)  getfslx(getData,  2, FSL_NONBLOCKING);
+        if (chan==3)  getfslx(getData,  3, FSL_NONBLOCKING);
+        if (chan==4)  getfslx(getData,  4, FSL_NONBLOCKING);
+        if (chan==5)  getfslx(getData,  5, FSL_NONBLOCKING);
+        if (chan==6)  getfslx(getData,  6, FSL_NONBLOCKING);
+        if (chan==7)  getfslx(getData,  7, FSL_NONBLOCKING);
+        if (chan==8)  getfslx(getData,  8, FSL_NONBLOCKING);
+        if (chan==9)  getfslx(getData,  9, FSL_NONBLOCKING);
+        if (chan==10) getfslx(getData, 10, FSL_NONBLOCKING);
+        if (chan==11) getfslx(getData, 11, FSL_NONBLOCKING);
+        if (chan==12) getfslx(getData, 12, FSL_NONBLOCKING);
+        if (chan==13) getfslx(getData, 13, FSL_NONBLOCKING);
+        if (chan==14) getfslx(getData, 14, FSL_NONBLOCKING);
+        if (chan==15) getfslx(getData, 15, FSL_NONBLOCKING);
+      }
       fsl_isinvalid(fslNrdy);
       fsl_iserror  (fslErr);
-      // printf("Reading word %d from FSL%d = %0lX, Empty = %0lX, Err = %0lX \n", word, chan, getData, fslNrdy, fslErr);
+      // printf("Reading word %d from FSL%d = %0lX, Empty = %d, Err = %d \n", word, chan, getData, fslNrdy, fslErr);
       if (fslNrdy || fslErr || getData!=putData) {
-        printf("\nERROR: Failed read of word %d from FSL%d = %0lX (expected %0lX), Empty = %0lX, Err = %0lX \n",
+        printf("\nERROR: Failed read of word %d from FSL%d = %0lX (expected %0lX), Empty = %d, Err = %d \n",
                word, chan, getData, putData, fslNrdy, fslErr);
         exit(1);
       }
