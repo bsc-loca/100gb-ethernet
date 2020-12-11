f1ca1779621363182312cb99bc508f035bae99e6
Alexander Kropotov <alexander.kropotov@bsc.es>
2020-12-11 14:48:27 +0100

    App: build tcl is extended with reporting and commented lwIP lib addition

--- tcl/build_app.tcl
+++ tcl/build_app.tcl
@@ -4,23 +4,60 @@
 
 #Set Vitis workspace
 setws ./xsct_ws
-#Create application project
+#Create application project (combined creation of platform, domain/bsp, project/app)
 app create -name eth_test -hw ./project/ethernet_test_wrapper.xsa -proc microblaze_0 -arch 32 -os standalone -lang c++ -template {Empty Application (C++)}
+# -os freertos10_xilinx
+
+#Create platform and domain/bsp
+# platform create -name eth_test_platform -hw ./project/ethernet_test_wrapper.xsa -proc microblaze_0 -arch 32 -os standalone
+#Create project/app
+# app create -name eth_test -platform eth_test_platform -lang c++ -template {Empty Application (C++)}
+
+#Report created platform
+platform list
+platform active
+platform report
+
+#Report set OS
+domain list
+domain active
+domain report
+
+#Report created BSP
+bsp listparams -proc
+bsp getos
+bsp listparams -os
+bsp getdrivers
+#config the BSP
+# bsp setlib -name lwip211 
+# bsp regenerate
+# bsp getlibs
+# bsp listparams -lib lwip211
 
 #Report created project
-platform list
-platform report
 sysproj list
 sysproj report eth_test_system
+
+#Report created app
 app list
 app report eth_test
-
-#Config the app
+#config the app
 importsources -name eth_test -path ./src/eth_test.cpp
 app config -name eth_test -set build-config release
-app config -name eth_test -get build-config
 app config -name eth_test -add compiler-misc {-std=c++17 -Wall -Og}
+# app config -name eth_test -add libraries lwip211
+#report app configs
+app config -name eth_test
+app config -name eth_test -get build-config
 app config -name eth_test -get compiler-misc
+app config -name eth_test -get compiler-optimization
+app config -name eth_test -get define-compiler-symbols
+app config -name eth_test -get include-path
+app config -name eth_test -get libraries
+app config -name eth_test -get library-search-path
+app config -name eth_test -get linker-misc
+app config -name eth_test -get linker-script
+app config -name eth_test -get undef-compiler-symbols
 
 #Build the app
 app clean all
