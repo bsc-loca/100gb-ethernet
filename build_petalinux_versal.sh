b6306b36042509fbc945d20b655d7df42f025b75
Alexander Kropotov <alexander.kropotov@bsc.es>
2022-01-03 10:48:38 +0100

    Versal design: SD1 periph added to make booting Petalinux upto some later stages.

--- build_petalinux_versal.sh
+++ build_petalinux_versal.sh
@@ -1,6 +1,9 @@
 
+#if a lack of disk space is met, /tmp/plnx_proj* temporary folders should be removed
 petalinux-create --type project --template versal --name plnx_proj
 cd ./plnx_proj/
 petalinux-config --get-hw-description ../project/ethernet_test_wrapper.xsa
 petalinux-build
 petalinux-package --boot --u-boot
+#the following is needed for case of EXT4 root filesystem type set in petalinux-config step (Image Packaging Configuration)  
+petalinux-package --wic
