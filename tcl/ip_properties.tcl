375670299ac074a32cd883b8722209c6f9123f54
Daniel J.Mazure <daniel.jimenez2@bsc.es>
2022-10-26 01:42:00 +0200

    Stick to 16650 for simplicity

--- tcl/ip_properties.tcl
+++ tcl/ip_properties.tcl
@@ -1,6 +1,6 @@
 set g_ip_version_min 0
 set g_ip_version_maj 1
 set g_ip_version ${g_ip_version_maj}.${g_ip_version_min}
-set g_ip_name "uart_16550a"
+set g_ip_name "uart_16650a"
 set g_ip_description "Modified UART IP from PULP"
 set g_display_name "Alveo MEEP-PULP UART" \
